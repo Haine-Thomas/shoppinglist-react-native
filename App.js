d5ac23946440819e9f343bd66b0905e8d4f3db76
Mushi2216 <thomahaine@gmail.com>
2020-10-06 17:29:12 +0200

    header and list + icon to delete ok

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem'
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: 1, text:'Milk'},
+    {id: 2, text:'Eggs'},
+    {id: 3, text:'Bread'},
+    {id: 4, text:'Juice'},
+
+  ]);
+
+  const deleteItem = () => {
+    
+  }
   return (
     <View style={styles.container}>
-      <Header title='Shopping List' />
+      <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem
+            item={item}
+          />
+      )}></FlatList>
     </View>
   );
 };
